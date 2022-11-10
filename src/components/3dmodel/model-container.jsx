4e67d05eaf49769f38d345002fd3f074c5e35434
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-11-10 09:26:05 +0300

    Update model-container.jsx

--- src/components/3dmodel/model-container.jsx
+++ src/components/3dmodel/model-container.jsx
@@ -1,21 +1,50 @@
-import { forwardRef } from 'react';
-import CatSpinner from '../ui/CatSpinner';
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+  useTexture,
+} from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { angleToRadians } from '../utils/angle';
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { useThree } from '@react-three/fiber';
+import { useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
+ 
+import { CarModel } from './car';
+import { CatModel } from './cat';
 
-export const ModelSpinner = () => <CatSpinner duration="1s" />;
+const Three = () => {
+  
 
-// eslint-disable-next-line react/display-name
-export const ModelContainer = forwardRef((props, ref) => (
-  <div ref={ref} className="model-container">
-    {props.children}
-  </div>
-));
-
-const Loader = () => {
   return (
-    <ModelContainer>
-      <ModelSpinner />
-    </ModelContainer>
+    <>
+      <PerspectiveCamera makeDefault position={[-5, 5, -15]} />
+      <OrbitControls ref={orbitControlsRef} />
+     
+      <CatModel
+        scale={0.15}
+        receiveShadow
+        castShadow
+        position={[2, 0, -4]}
+        rotation={[0, angleToRadians(-55), 0]}
+      />
+
+     
+      <ambientLight args={['#ffffff', 0.25]} />  
+      <spotLight
+        castShadow
+        args={['#ffffff', 5]}
+        position={[-15, 9, -5]}
+        penumbra={1}
+        distance={45}
+        decay={0.5}
+        angle={angleToRadians(45)}
+      />
+    
+    </>
   );
 };
 
-export default Loader;
+export default Three;
